5ac98579a2d53d89f2b179ec542746952dba2e3e
arya020595 <aryarifqipratama@gmail.com>
2026-01-12 12:00:36 +0700

    Implement user authentication with Devise and configure database settings

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :trackable
+end

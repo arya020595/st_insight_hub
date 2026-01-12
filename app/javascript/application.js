f6be2113ff2b2f81fea6d9ccc1abaf40bda41228
arya020595 <aryarifqipratama@gmail.com>
2026-01-12 17:57:57 +0700

    feat: Add application styles and Bootstrap integration for dashboard layout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import * as bootstrap from "bootstrap";
+import "controllers";
+
+// Make Bootstrap available globally for dropdown, collapse, etc.
+window.bootstrap = bootstrap;
